6cbe58596a5355d729f1a01cdcbf7260c179db4f
Daniel Valcarce <davamix@gmail.com>
2014-07-07 23:43:57 +0200

    Remove unused namespaces

--- CatsharpCollections.Common/IocConfiguration.cs
+++ CatsharpCollections.Common/IocConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autofac;
+﻿using Autofac;
 using CatsharpCollections.Contracts;
 
 namespace CatsharpCollections.Common
